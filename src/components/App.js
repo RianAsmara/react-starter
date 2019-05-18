3a5b3991689fc4ed91606d4b2d2d6f11fccdf19d
RianAsmara <gagahkalem@gmail.com>
2019-05-18 11:38:15 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    font-family: 'Open Sans', sans-serif;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 500px;
+    height: 40%;
+`;
+
+const Title = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
+
+const Paragraph = styled.p`
+    color: black;
+    font-size: 1rem;
+`;
+
+const Link = styled.a`
+    color: #00a8e8;
+`;
+
+const App = () => (
+    <Container>
+        <Wrapper>
+            <Title>Hello From Browser</Title>
+            <Paragraph>
+                ReactJS Webpack Babel Starter Pack
+            </Paragraph>
+        </Wrapper>
+    </Container>
+);
+
+export default App;
